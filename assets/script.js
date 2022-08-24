1bf1690038106e692fd8b7ce4e69dee3c0d534f8
davidcofe <104643151+davidcofe@users.noreply.github.com>
2022-08-24 16:22:52 -0500

    Add files via upload

---
+++ assets/script.js
@@ -0,0 +1,24 @@
+function action_1(){
+    document.getElementById("HomeText").hidden = false;
+    document.getElementById("ATText").hidden = true;
+    document.getElementById("KJText").hidden = true;
+    document.getElementById("TBLText").hidden = true;
+}
+function action_2(){
+    document.getElementById("HomeText").hidden = true;
+    document.getElementById("ATText").hidden = false;
+    document.getElementById("KJText").hidden = true;
+    document.getElementById("TBLText").hidden = true;
+}
+function action_3(){
+    document.getElementById("HomeText").hidden = true;
+    document.getElementById("ATText").hidden = true;
+    document.getElementById("KJText").hidden = false;
+    document.getElementById("TBLText").hidden = true;
+}
+function action_4(){
+    document.getElementById("HomeText").hidden = true;
+    document.getElementById("ATText").hidden = true;
+    document.getElementById("KJText").hidden = true;
+    document.getElementById("TBLText").hidden = false;
+}
